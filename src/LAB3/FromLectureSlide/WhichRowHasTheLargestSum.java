9104e1637e5fa36ded20d0e9d41311b203350560
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-06-22 21:50:53 +0600

    WhichRowHasTheLargestSum.java, Done.

---
+++ src/LAB3/FromLectureSlide/WhichRowHasTheLargestSum.java
@@ -0,0 +1,36 @@
+package LAB3.FromLectureSlide;
+
+import java.util.Scanner;
+
+public class WhichRowHasTheLargestSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the rows: ");
+        int n = scanner.nextInt();
+        System.out.print("Enter the columns: ");
+        int m = scanner.nextInt();
+
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        int largestSum = Integer.MIN_VALUE;
+        int largestSumRow = -1;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < m; j++) {
+                sum += arr[i][j];
+            }
+            System.out.println("Sum of row " + i + " = " + sum);
+            if (sum > largestSum) {
+                largestSum = sum;
+                largestSumRow = i;
+            }
+        }
+        System.out.println("Row " + largestSumRow + " has the largest sum = " + largestSum);
+    }
+}
