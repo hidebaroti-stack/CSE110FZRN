25b3cd20f7942a9c6f3d5073b48388ecc21a43e2
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-07-31 21:18:01 +0600

    WrapperClassesVSPrimitiveTypes.java, Done.

---
+++ src/FZRN_SLIDES/Generics/WrapperClassesVSPrimitiveTypes.java
@@ -0,0 +1,14 @@
+package FZRN_SLIDES.Generics;
+
+public class WrapperClassesVSPrimitiveTypes {
+    public static void main(String[] args) {
+        // Primitive
+        int num = 5;
+
+        // Wrapper
+        Integer obj = 5;
+
+        System.out.println("Primitive: " + num);
+        System.out.println("Wrapper: " + obj);
+    }
+}
